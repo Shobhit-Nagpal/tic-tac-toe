67825f7be781a74d55f21aa19303b9e52c06c433
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2022-10-16 21:42:38 +0530

    Added functionality to add symbols on click to tiles

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let currentSymbol = 'X';
+
 //Module to check gameboard
 const gameBoard = (() => {
     let gameboard = [];
@@ -5,9 +7,35 @@ const gameBoard = (() => {
 
 //Module to check game flow
 const gameFlow = (() => {
-
+    const checkWinner = () => {
+        
+    }
 })();
 
+//Factory function for players
 const players = (name, symbol) => {
     return {name, symbol};
 }
+
+const player1 = players('Player 1', 'X');
+const player2 = players('Player 2', 'O');
+
+
+//Logic for filling the tiles with symbols
+const tiles = document.querySelectorAll('.gametiles');
+
+tiles.forEach((tile) => {
+    tile.addEventListener('click', () => {
+        if (tile.textContent === '' && currentSymbol === 'X') {
+            tile.textContent = 'X';
+            currentSymbol = 'O';
+        }
+        else if (tile.textContent === '' && currentSymbol === 'O') {
+            tile.textContent = 'O';
+            currentSymbol = 'X';
+        }
+        else {
+            return;
+        }
+    })
+})
