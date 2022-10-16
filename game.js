1e292385e347e8d23632c1a2ebd58a9500dbef70
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2022-10-16 21:19:06 +0530

    Added modules and factory funtions for gameboard and players

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("Hi console");
+//Module to check gameboard
+const gameBoard = (() => {
+    let gameboard = [];
+})();
+
+//Module to check game flow
+const gameFlow = (() => {
+
+})();
+
+const players = (name, symbol) => {
+    return {name, symbol};
+}
